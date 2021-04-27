f38fafa60b56a941d9a1d7358453dff389f15d96
DucAnh <nguyenducanh.ldb@gmail.com>
2021-04-27 09:51:57 +0700

    update for backup

--- controller/novel.controller.js
+++ controller/novel.controller.js
@@ -1,8 +1,7 @@
 const db = require('../db.js')
+const shortid = require('shortid');
 
 module.exports.showData = function(req, res) {
-	console.log(db.get('users').value());
-
 	res.render('novel/show', {
 		novels: db.get('novels').value()
 	});
@@ -13,8 +12,34 @@ module.exports.create = function(req, res) {
 }
 
 module.exports.postCreate = function(req, res) {
+	req.body.id = shortid.generate();
 	req.body.avatar = req.file.path.split('/').slice(1).join('/');
 
 	db.get('novels').push(req.body).write();
 	res.redirect('/novel/show');
+}
+
+module.exports.view = function(req, res) {
+	var id = req.params.id;
+
+	var novel = db.get('novels').find({ id: id }).value();
+
+	res.render('novel/viewMore', {
+		novel: novel
+	});
+}
+
+module.exports.search = function(req, res) {
+	var input = req.query.q;
+
+	var novels = db.get('novels').value();
+
+	var filteredNovels = novels.filter(function(novel) {
+		return novel.name.toLowerCase().indexOf(input.toLowerCase()) !== -1;
+	});
+
+	res.render('novel/show', {
+		novels: filteredNovels, 
+		input: input
+	})
 }
--- route/novel.router.js
+++ route/novel.router.js
@@ -1,20 +1,29 @@
 const express = require('express')
 
-// La middleware su dung de upload files
+// This is middleware use upload file 
 var multer  = require('multer')
 
 const controller = require('../controller/novel.controller')
 
 var router = express.Router()
 
-// Files dc upload duoc luu trong thu mux uploads
+// File upload to folder uploads
 var upload = multer({ dest: './public/uploads/' })
 
+// Showed novels
 router.get('/show', controller.showData);
 
+// Create a novel and add to db
 router.get('/create', controller.create);
 
-// upload.single de upload 1 file
+// upload.single use load a single file
 router.post('/create', upload.single('avatar'), controller.postCreate);
+
+// use to search novel from name
+router.get('/search', controller.search);
+
+// use route parameter can to take much id 
+router.get('/:id', controller.view);
+
 
 module.exports = router;
